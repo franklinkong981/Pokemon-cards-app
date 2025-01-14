34063f10a2017eb2677b748774b4dfe54afe8e8e
Franklin Kong <franklinkong981@gmail.com>
2025-01-13 21:28:10 -0800

    Modified useAxios hook so it can be used by PokeDex component

--- pokemon-cards-app/src/hooks/useAxios.jsx
+++ pokemon-cards-app/src/hooks/useAxios.jsx
@@ -2,12 +2,14 @@ import React, {useState} from "react";
 import {v1 as uuid} from "uuid";
 import axios from "axios";
 
-const useAxios = (initialValue = []) => {
+//if isPokemon is false, will call the Deck of Cards API. If true, will call the Pokemon API.
+const useAxios = (isPokemon, initialValue = []) => {
   const [cards, setCards] = useState(initialValue);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
+  const BASE_URL = isPokemon ? `https://pokeapi.co/api/v2/pokemon/` : "https://deckofcardsapi.com/api/deck/new/draw/";
+
+  const addCard = async (pokemonName = "") => {
+    const url = isPokemon ? BASE_URL + `${pokemonName}/` : BASE_URL;
+    const response = await axios.get(url);
     setCards(cards => [...cards, { ...response.data, id: uuid() }]);
   };
 
